982de5a350c3045f420543a7e6d9672c07dd5d8b
Rodney Blevins <rodney@blevins.nl>
2018-07-17 17:02:07 -0600

    switch to php

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once 'includes/startup.inc';
+
+$json = array();
+// parse the post header before loading shared.inc
+$postObject = array();
+if (empty($_POST) && file_get_contents("php://input")) {
+	$headerInput = file_get_contents("php://input");
+	$postObject = json_decode($headerInput,true);
+	if (json_last_error() != JSON_error_NONE) {
+		$json['status'] = "error";
+		$json['message'] = "JSON format error from posted input: " . json_last_error_msg();
+		echo json_encode($json);
+		exit;
+	}
+} else {
+	$postObject = $_POST;
+}
+
+$DBConnection = new mysqli("mysql.fantagul.us", "fantagulus", "TUKwEnDZ", "fantagul_us");
+
+// print_r($DBConnection);
+
+$path = explode("/", $_GET['path']);
+
+switch ($path[1]) {
+	case "v1":
+		require_once $path[1] . "/api.php";
+		break;
+	default:
+		http_response_code(404);
+		include 'includes/code_404.php';
+		die();
+}
+
+mysqli_close($DBConnection);
+echo json_encode($json);
+exit;
+?>
