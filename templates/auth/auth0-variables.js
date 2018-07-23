e4e32f25139a0031f691ca8a1ef409e56bb4ea83
reblevins <rodney@blevins.nl>
2018-07-22 20:22:41 -0600

    Update auth0-variables.js

--- templates/auth/auth0-variables.js
+++ templates/auth/auth0-variables.js
@@ -1,6 +1,4 @@
 export const AUTH_CONFIG = {
-  clientId: 'uFRIXilpxCkWbpjqRgA9wln1rtP59iRI',
-  domain: 'fantagulus.auth0.com',
   callbackUrl: 'http://localhost:3000/callback',
   apiUrl: 'API_IDENTIFIER'
 }
